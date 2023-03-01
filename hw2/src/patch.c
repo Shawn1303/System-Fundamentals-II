833489684d2a7ee1ea7d803704e679ed0438286b
(Shawn) Jia Cheng Zhu <jiachzhu@cs.stonybrook.edu>
2023-02-28 19:40:58 -0500

    changed the free(i_womp)

--- hw2/src/patch.c
+++ hw2/src/patch.c
@@ -954,8 +954,10 @@ void re_input()
 		i_size = 0;
 		/*NOSTRICT*/
 		if (i_ptr != Null(char **)){
-			free((char *)i_womp);
 			free((char *)i_ptr);
+		}
+		if (i_womp != Nullch) {
+			free((char *)i_womp);
 		}
 		i_womp = Nullch;
 		i_ptr = Null(char **);
